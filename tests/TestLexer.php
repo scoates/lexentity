5ead42e003c675a37eb9f286e430441159742cf7
Sean Coates <sean@seancoates.com>
2008-12-27 17:50:35 +0000

    lexentity

---
+++ tests/TestLexer.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once 'PHPUnit/Framework.php';
+require_once dirname(__FILE__) . '/../inc/Lexer.php';
+
+class UserControllerTest extends PHPUnit_Framework_TestCase
+{
+    const SIMPLETEXT = 'This is O\'Reilly\'s book, entitled "Foo <em class="foo">Bars</em> for Baz!"';
+    const COMPLEXTEXT = 'O\'Reilly\'s book, <code>Don\'t touch this: "Foo <em class="foo">Bars</em></code> for Baz!"';
+    
+    public function testSplit()
+    {
+        $Lexer = new LexEntity\Lexer(self::SIMPLETEXT);
+        $this->assertEquals(12, count($Lexer->chunks));
+        $this->assertEquals(self::SIMPLETEXT, implode('', $Lexer->chunks));
+    }
+    
+    public function testSet()
+    {
+        $set = LexEntity\Token\Set::getInstance(new LexEntity\Lexer(self::SIMPLETEXT));
+        $this->assertTrue($set instanceof Lexentity\Token\Set);
+    }
+    
+    public function testTranslation()
+    {
+        $set = LexEntity\Token\Set::getInstance(new LexEntity\Lexer(self::SIMPLETEXT));
+        $this->assertEquals('This is O&#8217;Reilly&#8217;s book, entitled &#8220;Foo <em class="foo">Bars</em> for Baz!&#8221;', $set->asString());
+    }
+    
+    public function testTranslationComplex()
+    {
+        $set = LexEntity\Token\Set::getInstance(new LexEntity\Lexer(self::COMPLEXTEXT));
+        echo $set->asString();
+        return;
+        $this->assertEquals('This is O&#8217;Reilly&#8217;s book, entitled &#8220;Foo <em class="foo">Bars</em> for Baz!&#8221;', $set->asString());
+    }
+}
